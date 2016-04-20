6f6fdfee755b96f430c514e1678d5ba7a1c9c715
Benjamin Komen <benjamin_komen@hotmail.com>
2016-04-20 12:15:55 +0200

    more testing return variables

--- graphs/render_ansys_fft_data.js
+++ graphs/render_ansys_fft_data.js
@@ -139,8 +139,10 @@ $(document).ready(function() {
 			console.log(options.series);
 			console.log('1 series names are:' + series_names[0] + series_names[1]);
 			var chart = new Highcharts.Chart(options);
-			},
+			return series_names;
+		},
 		"text");
+		return series_names;
 		});
 	console.log('2 series names are:' + series_names[0] + series_names[1]);
 	//loop through fft data files and preprocess data to plot graph
