873a737abc0a63d62951ad909cbfdf69c9941e89
Benjamin Komen <benjamin_komen@hotmail.com>
2016-04-25 09:47:24 +0200

    adding another 2 hht runs with different gamma

--- graphs/fft/fft_script_ansys_input.m
+++ graphs/fft/fft_script_ansys_input.m
@@ -10,7 +10,7 @@ clc;
 % read data into array using third-party script
 file_name	= '3';
 file_ext	= '.txt';
-file_path	= '../ansys-data/vehicle_full_coach/';
+file_path	= '../ansys-data/vehicle_full_coach_ne_90/';
 input_path	= strcat(file_path, file_name, file_ext);
 [data_array,ffn,nh,SR,hl]	= txt2mat(input_path);
 
