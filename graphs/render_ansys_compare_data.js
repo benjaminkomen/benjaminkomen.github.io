c5c9bf00dc98504f3962b7bc10324d8559366a63
Benjamin Komen <benjamin_komen@hotmail.com>
2016-04-27 13:13:02 +0200

    add exception for undefined

--- graphs/render_ansys_compare_data.js
+++ graphs/render_ansys_compare_data.js
@@ -9,13 +9,18 @@ $(document).ready(function() {
     if (!url) url = window.location.href;
     url = url.toLowerCase(); // This is just to avoid case sensitiveness
 	console.log(url);
-    name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
-	console.log(name);
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	if (name !== "" && name !== null && name != undefined) {
+		name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
+		console.log(name);
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	} else {
+        var arr = location.href.split("/");
+        return arr[arr.length - 1];
+    }
 	}
 
 	// Array of deferred objects
